14e8deefcd7fad84a20f7e596f735fd5b815f861
nilajcoder <nilaj.chakraborty007@gmail.com>
2025-09-10 22:40:08 +0530

    ImageInsert

--- work/Day One/text/README.md
+++ work/Day One/text/README.md
@@ -1,5 +1,20 @@
 
 
-## Task 2
+## Task 2 Task 6
 
 Create Different Text Format
+
+
+Insert Image using Image Component
+Use require for local Image and Uri for Online Pic
+
+
+
+✨ Key Learnings:
+require() ➝ Used to display local images from the project folder.
+uri ➝ Used to fetch and show online images via URL.
+Styling images with height, width, borderRadius, borderColor, and borderWidth makes them look clean and professional.
+🖼️ I created a small demo where I displayed:
+ ✔ A local React logo using require
+ ✔ An online image using uri
+ ✔ Applied rounded corners & borders for better UI
--- work/Day One/text/app/index.tsx
+++ work/Day One/text/app/index.tsx
@@ -1,11 +1,26 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View,Image, TouchableOpacity} from 'react-native'
 import React from 'react'
 
-const index = () => {
-  return (
-      
+const index = () =>{
 
+     const btnpress=()=>{
+    
+    alert("Thank You For Clicking the button")
+
+   }
+  return (
+
+     
        <View style={styles.container}>
+       <Text style={styles.title}>💻 Homework Day-6 💻</Text>
+
+       <Image source={require('../assets/images/react-logo.png')}></Image>
+       <Image source={{uri:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRyZIU9b9CdRoc6nDomWlvwYBlMh0IsYqz9cNJB4qNhudEUTBPl3e-13PFm23-C_gkN81s&usqp=CAU"}} style={styles.img}></Image>
+
+         <TouchableOpacity style={styles.button} activeOpacity={0.5} onPress={btnpress}>
+           <Image source={{uri:"https://www.shutterstock.com/image-vector/single-mouse-click-260nw-1045842496.jpg"}} style={styles.img1}></Image>
+          <Text style={styles.buttonText}>Click Me</Text>
+        </TouchableOpacity>
       <Text style={styles.textRedBig}>Welcome To Nilaj IT Company</Text>
       <Text style={styles.textGreenBig}>Welcome To Nilaj IT Company</Text>
       <Text style={styles.textPurpleBig}>Welcome To Nilaj IT Company</Text>
@@ -66,6 +81,52 @@ const styles = StyleSheet.create({
     fontSize: 20,
     marginVertical: 5,
   },
+
+  img: {
+
+    height:100,
+    width:100,
+    borderRadius:50,
+      borderWidth: 3,       // thickness of border
+  borderColor: 'black', // border color
+  } ,
+    title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: 'purple',
+    marginBottom: 20,
+    borderRadius:5,
+    borderColor:'black',
+    borderWidth:5,
+    
+  },
+
+  button :{
+     backgroundColor: 'purple',
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 10,
+    alignItems:"center", //centre
+    justifyContent:"center",//centre buttton
+
+    width:200,
+    alignSelf:"center" 
+  } ,
+
+    buttonText: {
+    color: 'white',
+    fontSize: 18,
+    fontWeight: 'bold',
+    textAlign:"center"
+    
+  },
+
+  img1:{
+
+    height:20,
+    width: 20
+  }
+
 
 
 })
